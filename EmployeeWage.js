042375944dd05460434ada2495c2826e2ecc8b7f
Saianjan <saianjan1234@gmail.com>
2023-12-07 11:37:42 +0530

    UC1 Completed and Added Employee Details with random Attendance!

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,1 +1,41 @@
 console.log("Welcome to Employee Wage Computation");
+let employeeDetails =[
+    {
+        eid:101,
+        name:"SaiAnjan",
+        attendance:null,
+        workhrs:null,
+        dailywage:null
+    },
+    {
+        eid:102,
+        name:"Yogesh",
+        attendance:null,
+        workhrs:null,
+        dailywage:null
+    },
+    {
+        eid:103,
+        name:"Uday",
+        attendance:null,
+        workhrs:null,
+        dailywage:null
+    },
+    {
+        eid:104,
+        name:"Madhu",
+        attendance:null,
+        workhrs:null,
+        dailywage:null
+    }
+]
+
+function attendanceCheck(){
+    employeeDetails.forEach((ele)=>{
+        if (ele.attendance==null) {
+            ele.attendance=Math.round(Math.random())
+            console.log(ele.attendance);
+        }
+    })
+}
+attendanceCheck()
