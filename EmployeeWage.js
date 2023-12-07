809290eb48bfd1ebaa67e31fb6ace64ab0dd4140
Saianjan <saianjan1234@gmail.com>
2023-12-07 12:48:03 +0530

    Welcome to First Commit!

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,1 +1,22 @@
 console.log("Welcome to Employee Wage Computation");
+let employeeDetails =[
+    {
+        eid:101,
+        name:"SaiAnjan"
+    },
+    {
+        eid:102,
+        name:"Yogesh"
+    },
+    {
+        eid:103,
+        name:"Uday"
+    },
+    {
+        eid:104,
+        name:"Madhu"
+    }
+]
+
+
+console.log(employeeDetails);
