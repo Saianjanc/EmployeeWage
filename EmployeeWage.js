2ccd4479a859720f80ef4df2536980602e457e89
Saianjan <saianjan1234@gmail.com>
2023-12-07 12:39:10 +0530

    UC2 Completed and Added Employee Daily Wage with random Work Hours!

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -20,10 +20,26 @@ let employeeDetails =[
 
 function attendanceCheck(){
     employeeDetails.forEach((ele)=>{
+
         const attendance=Math.round(Math.random())
         attendance ? ele.attendance="Present":ele.attendance="Absent"
     })
-    console.log(employeeDetails);
 }
-attendanceCheck()
 
+function computeWage(){
+    employeeDetails.forEach((ele)=>{
+        if (ele.attendance=="Present"){
+            ele.workHrs=Math.floor(Math.random()*(8-1)+1)
+            ele.dailyWage=ele.workHrs*20
+        }
+        else{
+            ele.workHrs=0
+            ele.dailyWage=0
+        }
+    })
+}
+
+attendanceCheck()
+computeWage()
+
+console.log(employeeDetails);
